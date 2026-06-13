922a58cbb6b96867a5662a923718b02ecdbc07a9
T-017 <jholt2@byupathway.edu>
2026-06-12 22:52:41 -0500

    problem 4

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -50,6 +50,8 @@ public class Node
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left is null ? 0 : Left.GetHeight();
+        int rightHeight = Right is null ? 0 : Right.GetHeight();
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
